daf7cf6f70ae4bc593fa8e97915d1a19e5466dc6
jermz678 <jermz678@live.com>
2021-03-15 16:39:57 -0500

    add event prevent

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
-var buttonE1 = document.querySelector("#save-task");
-console.log(buttonE1)
+var formEl =document.querySelector("#task-form");
+
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "this is a new task.";
     tasksToDoEl.appendChild(listItemEl);
+    console.log(event);
 };
 
-buttonE1.addEventListener("click", createTaskHandler);
+formEl.addEventListener("click", createTaskHandler);
